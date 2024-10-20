a76bbb5ef7880a932067de76e105855b2854269d
Akyl <akyl@gmail.com>
2024-10-20 21:08:07 +0200

    from 72 to 80

--- ML_with_Pytorch_and_scikit_learn/page_55.py
+++ ML_with_Pytorch_and_scikit_learn/page_55.py
@@ -244,20 +244,142 @@ class AdalineGD:
 
 fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(10, 4))
 ada1 = AdalineGD(n_iter=15, eta=0.1).fit(X, y)
-ax[0].plot(range(1, len(ada1.losses_) + 1),
-np.log10(ada1.losses_), marker='o')
-ax[0].set_xlabel('Epochs')
-ax[0].set_ylabel('log(Mean squared error)')
-ax[0].set_title('Adaline - Learning rate 0.1')
-ada2 = AdalineGD(n_iter=15, eta=0.0001).fit(X, y)
-ax[1].plot(range(1, len(ada2.losses_) + 1),
-ada2.losses_, marker='o')
-ax[1].set_xlabel('Epochs')
-ax[1].set_ylabel('Mean squared error')
-ax[1].set_title('Adaline - Learning rate 0.0001')
+# ax[0].plot(range(1, len(ada1.losses_) + 1),
+# np.log10(ada1.losses_), marker='o')
+# ax[0].set_xlabel('Epochs')
+# ax[0].set_ylabel('log(Mean squared error)')
+# ax[0].set_title('Adaline - Learning rate 0.1')
+# ada2 = AdalineGD(n_iter=15, eta=0.0001).fit(X, y)
+# ax[1].plot(range(1, len(ada2.losses_) + 1),
+# ada2.losses_, marker='o')
+# ax[1].set_xlabel('Epochs')
+# ax[1].set_ylabel('Mean squared error')
+# ax[1].set_title('Adaline - Learning rate 0.0001')
+# plt.show()
+
+# Standardization can easily be achieved by using the built-in NumPy methods mean and std:
+X_std = np.copy(X)
+X_std[:,0] = (X[:,0] - X[:,0].mean()) / X[:,0].std()
+X_std[:,1] = (X[:,1] - X[:,1].mean()) / X[:,1].std()
+
+class AdalineSGD:
+    """ADAptive LInear NEuron classifier.
+        Parameters
+        ------------
+        eta : float
+            Learning rate (between 0.0 and 1.0)
+        n_iter : int
+            Passes over the training dataset.
+        shuffle : bool (default: True)
+            Shuffles training data every epoch if True to prevent
+            cycles.
+        random_state : int
+            Random number generator seed for random weight
+            initialization.
+        Attributes
+        -----------
+        w_ : 1d-array
+            Weights after fitting.
+        b_ : Scalar
+            Bias unit after fitting.
+        losses_ : list
+            Mean squared error loss function value averaged over all
+            training examples in each epoch.
+    """
+    def __init__(self, eta=0.01, n_iter=10, shuffle=True, random_state=None):
+        self.eta = eta
+        self.n_iter = n_iter
+        self.w_initialized = False
+        self.shuffle = shuffle
+        self.random_state = random_state
+
+    def fit(self, X, y):
+        """ Fit training data.
+            Parameters
+            ----------
+            X : {array-like}, shape = [n_examples, n_features]
+                Training vectors, where n_examples is the number of
+                examples and n_features is the number of features.
+            y : array-like, shape = [n_examples]
+                Target values.
+            Returns
+            -------
+            self : object
+        """
+        self._initialize_weights(X.shape[1])
+        self.losses_ = []
+        for i in range(self.n_iter):
+            if self.shuffle:
+                X,y = self._shuffle(X, y)
+            losses = []
+            for xi, target in zip(X, y):
+                losses.append(self._update_weights(xi, target))
+            avg_loss = np.mean(losses)
+            self.losses_.append(avg_loss)
+
+        return self
+
+    def partial_fit(self, X, y):
+        """Fit training data without reinitializing the weights"""
+        if not self.w_initialized:
+            self._initialize_weights(X.shape[1])
+        if y.reval().shape[0] > 1:
+            for xi, target in zip(X, y):
+                self._update_weights(xi, target)
+
+        else:
+            self._update_weights(X, y)
+
+        return self
+
+    def _shuffle(self, X, y):
+        """shuffle training data"""
+        r = self.rgen.permutation(len(y))
+        return X[r], y[r]
+
+    def _initialize_weights(self, m):
+        """Initialize weights to small random numbers"""
+        self.rgen = np.random.RandomState(self.random_state)
+        self.w_ = self.rgen.normal(loc=0.0, scale=0.01,size=m)
+        self.b_ = np.float_(0.)
+        self.w_initialized = True
+
+    def _update_weights(self, xi, target):
+        """Apply Adaline learning rule to update the weights"""
+        output = self.activation(self.net_input(xi))
+        error = (target - output)
+        self.w_ += self.eta * 2.0 * xi * (error)
+        self.b_ += self.eta * 2.0 * (error)
+        loss = error ** 2
+        return loss
+
+    def net_input(self, X):
+        """Calculate net input"""
+        return np.dot(X, self.w_) + self.b_
+
+    def activation(self, X):
+        """compute linear activation"""
+        return X
+
+    def predict(self, X):
+        """Return class label after unit step"""
+        return np.where(self.activation(self.net_input(X)) >= 0.5, 1, 0)
+
+
+ada_sgd = AdalineSGD(n_iter=15, eta=0.01, random_state=1)
+ada_sgd.fit(X_std, y)
+plot_decision_regions(X_std, y, classifier=ada_sgd)
+plt.title('Adaline - Stochastic gradient descent')
+plt.xlabel('Sepal length [standardized]')
+plt.ylabel('Petal length [standardized]')
+plt.legend(loc='upper left')
+plt.tight_layout()
 plt.show()
-
-
+plt.plot(range(1, len(ada_sgd.losses_)+1), ada_sgd.losses_, marker='o')
+plt.xlabel('Epochs')
+plt.ylabel('Average loss')
+plt.tight_layout()
+plt.show()
 
 
 
