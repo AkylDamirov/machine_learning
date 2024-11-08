e88e8bfc46c05537a418826c83685bc2f7bb8cd5
Akyl <akyl@gmail.com>
2024-11-08 23:49:02 +0100

    from 100 to 115

--- ML_with_Pytorch_and_scikit_learn/page_82.py
+++ ML_with_Pytorch_and_scikit_learn/page_82.py
@@ -193,12 +193,98 @@ class LogisticRegressionGD:
 # plt.show()
 
 from sklearn .linear_model import LogisticRegression
-lr = LogisticRegression(C=100.0, solver='lbfgs', multi_class='ovr')
-lr.fit(X_train_std, y_train)
-plot_decision_regions(X_combined_std, y_combined, classifier=lr, test_idx=range(105, 150))
+# lr = LogisticRegression(C=100.0, solver='lbfgs', multi_class='ovr')
+# lr.fit(X_train_std, y_train)
+# plot_decision_regions(X_combined_std, y_combined, classifier=lr, test_idx=range(105, 150))
+# plt.legend(loc='upper left')
+# plt.tight_layout()
+# plt.show()
+# print(lr.predict_proba(X_test_std[:3, :]))
+
+# weights, params = [], []
+# for c in np.arange(-5, 5):
+#     lr = LogisticRegression(C=10.**c, multi_class='ovr')
+#     lr.fit(X_train_std, y_train)
+#     weights.append(lr.coef_[1])
+#     params.append(10.**c)
+# weights = np.array(weights)
+# plt.plot(params, weights[:, 0], label='Petal length')
+# plt.plot(params, weights[:, 1], linestyle='--', label='Petal width')
+# plt.ylabel('Weight coefficient')
+# plt.xlabel('C')
+# plt.legend(loc='upper left')
+# plt.xscale('log')
+# plt.show()
+
+from sklearn.svm import SVC
+# svm = SVC(kernel='linear', C=1, random_state=1)
+# svm.fit(X_train_std, y_train)
+# plot_decision_regions(X_combined_std, y_combined, classifier=svm, test_idx=range(105, 150))
+# plt.xlabel('Petal length [standardized]')
+# plt.ylabel('Petal width [standardized]')
+# plt.legend(loc='upper left')
+# plt.tight_layout()
+# plt.show()
+
+from sklearn.linear_model import SGDClassifier
+ppn = SGDClassifier(loss='perceptron')
+lr = SGDClassifier(loss='log')
+svm = SGDClassifier(loss='hinge')
+
+import numpy as np
+np.random.seed(1)
+X_xor = np.random.randn(200, 2)
+y_xor = np.logical_xor(X_xor[:, 0] > 0, X_xor[:, 1] > 0)
+y_xor = np.where(y_xor, 1, 0)
+# plt.scatter(X_xor[y_xor==1, 0],
+#             X_xor[y_xor==1, 1],
+#             c='royalblue', marker='s',
+#             label='Class 1')
+# plt.scatter(X_xor[y_xor == 0,0],
+#             X_xor[y_xor==0,1],
+#             c='tomato', marker='o',
+#             label='Class 0')
+# plt.xlim([-3, 3])
+# plt.ylim([-3, 3])
+# plt.xlabel('Feature 1')
+# plt.ylabel('Feature 2')
+# plt.legend(loc='best')
+# plt.tight_layout()
+# plt.show()
+
+# svm = SVC(kernel='rbf', random_state=1, gamma=0.10, C=10.0)
+# svm.fit(X_xor, y_xor)
+# plot_decision_regions(X_xor, y_xor, classifier=svm)
+# plt.legend(loc='upper left')
+# plt.tight_layout()
+# plt.show()
+
+# svm = SVC(kernel='rbf', random_state=1, gamma=0.2, C=1.0)
+# svm.fit(X_train_std, y_train)
+# plot_decision_regions(X_combined_std, y_combined, classifier=svm, test_idx=range(105, 150))
+# plt.xlabel('Petal length [standardized]')
+# plt.ylabel('Petal width [standardized]')
+# plt.legend(loc='upper left')
+# plt.tight_layout()
+# plt.show()
+
+svm = SVC(kernel='rbf', random_state=1, gamma=100.0, C=1.0)
+svm.fit(X_train_std, y_train)
+plot_decision_regions(X_combined_std, y_combined, classifier=svm, test_idx=range(105, 150))
+plt.xlabel('Petal length [standardized]')
+plt.ylabel('Petal width [standardized]')
 plt.legend(loc='upper left')
 plt.tight_layout()
 plt.show()
+
+
+
+
+
+
+
+
+
 
 
 
