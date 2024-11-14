50a65a45cdd4a4d8fa6446efcdb71333b94a4128
Akyl <akyl@gmail.com>
2024-11-14 17:34:26 +0100

    from 115 to 132

--- ML_with_Pytorch_and_scikit_learn/page_82.py
+++ ML_with_Pytorch_and_scikit_learn/page_82.py
@@ -268,21 +268,98 @@ y_xor = np.where(y_xor, 1, 0)
 # plt.tight_layout()
 # plt.show()
 
-svm = SVC(kernel='rbf', random_state=1, gamma=100.0, C=1.0)
-svm.fit(X_train_std, y_train)
-plot_decision_regions(X_combined_std, y_combined, classifier=svm, test_idx=range(105, 150))
+# svm = SVC(kernel='rbf', random_state=1, gamma=100.0, C=1.0)
+# svm.fit(X_train_std, y_train)
+# plot_decision_regions(X_combined_std, y_combined, classifier=svm, test_idx=range(105, 150))
+# plt.xlabel('Petal length [standardized]')
+# plt.ylabel('Petal width [standardized]')
+# plt.legend(loc='upper left')
+# plt.tight_layout()
+# plt.show()
+
+# def entropy(p):
+#     return -p * np.log2(p) - (1 - p) * np.log2((1-p))
+# x = np.arange(0.0, 1.0, 0.01)
+# ent = [entropy(p) if p!=0 else None for p in x]
+# plt.ylabel('Entropy')
+# plt.xlabel('Class membership Probability p(i=1)')
+# plt.plot(x, ent)
+# plt.show()
+
+# def gini(p):
+#     return p*(1-p) + (1 - p)*(1-(1-p))
+# def entropy(p):
+#     return -p * np.log2(p) - (1 - p) * np.log2(1-p)
+# def error(p):
+#     return 1 - np.max([p, 1-p])
+# x = np.arange(0.0, 1.0, 0.01)
+# ent = [entropy(p) if p!=0 else None for p in x]
+# sc_ent = [e*0.5 if e else None for e in ent]
+# err = [error(i) for i in x]
+# fig = plt.figure()
+# ax = plt.subplot(111)
+#
+# for i, lab, ls,c in zip([ent, sc_ent, gini(x), err],
+#                         ['Entropy', 'Entropy (scaled)', 'Gini impurity', 'Misclassification error'],
+#                         ['-', '-', '--', '-.'],
+#                         ['black', 'lightgray', 'red', 'green', 'cyan']):
+#     line = ax.plot(x, i, label=lab,
+#                    linestyle=ls, lw=2, color=c)
+#
+# ax.legend(loc='upper center', bbox_to_anchor=(0.5, 1.15), ncol=5,
+#           fancybox=True, shadow=False)
+# ax.axhline(y=0.5, linewidth=1, color='k', linestyle='--')
+# ax.axhline(y=1.0, linewidth=1, color='k', linestyle='--')
+# plt.ylim([0, 1.1])
+# plt.xlabel('p(i=1)')
+# plt.ylabel('impurity index')
+# plt.show()
+#
+
+from sklearn.tree import DecisionTreeClassifier
+tree_model = DecisionTreeClassifier(criterion='gini',
+                                    max_depth=3,
+                                    random_state=1)
+
+tree_model.fit(X_train, y_train)
+X_combined = np.vstack((X_train, X_test))
+y_combined = np.hstack((y_train, y_test))
+# plot_decision_regions(X_combined,
+#                       y_combined,
+#                       classifier=tree_model,
+#                       test_idx=range(105, 150))
+# plt.xlabel('Petal length [cm]')
+# plt.ylabel('Petal width [cm]')
+# plt.legend(loc='upper left')
+# plt.tight_layout()
+# plt.show()
+
+# from sklearn import tree
+# feature_names = ['Sepal length', 'Sepal width', 'Petal length', 'Petal width']
+# tree.plot_tree(tree_model, feature_names=feature_names, filled=True)
+# plt.show()
+
+from sklearn.ensemble import RandomForestClassifier
+
+# forest = RandomForestClassifier(n_estimators=25, random_state=1, n_jobs=2)
+# forest.fit(X_train, y_train)
+# plot_decision_regions(X_combined, y_combined, classifier=forest, test_idx=range(105, 150))
+# plt.xlabel('Petal length [cm]')
+# plt.ylabel('Petal width [cm]')
+# plt.legend(loc='upper left')
+# plt.tight_layout()
+# plt.show()
+
+from sklearn.neighbors import KNeighborsClassifier
+
+knn = KNeighborsClassifier(n_neighbors=5,p=2,metric='minkowski')
+knn.fit(X_train_std, y_train)
+plot_decision_regions(X_combined_std, y_combined, classifier=knn, test_idx=range(105, 150))
 plt.xlabel('Petal length [standardized]')
 plt.ylabel('Petal width [standardized]')
 plt.legend(loc='upper left')
 plt.tight_layout()
 plt.show()
-
-
-
-
-
-
-
 
 
 
